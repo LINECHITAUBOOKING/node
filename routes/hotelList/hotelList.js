6d78dcf03f12df543c8f5ac26fcd615d4464ef1d
Echelon0815 <jack.w886@gmail.com>
2023-02-06 12:30:43 +0800

    新增trip-list用的api

--- routes/hotelList/hotelList.js
+++ routes/hotelList/hotelList.js
@@ -3,11 +3,8 @@ const router = express.Router()
 
 const pool = require('../../utils/db')
 
-router.get('/:region', async (req, res, next) => {
-  let [data] = await pool.query(
-    'SELECT hotel_account.*, hotel_service_list.* FROM hotel_account INNER JOIN hotel_service_list ON hotel_account.company_name=hotel_service_list.hotel WHERE hotel_account.region=?',
-    [req.params.region]
-  )
+router.get('/:hotelName', async (req, res, next) => {
+  let [data] = await pool.query('SELECT * FROM hotel_account')
   res.json(data)
 })
 
