273e370436b7a180b76e688337d3012aa28cfb4d
Lu-PY <lpy102817@gmail.com>
2023-02-08 21:01:39 +0800

    feat:優惠券使用

--- routes/payment/Detail/Hotel.js
+++ routes/payment/Detail/Hotel.js
@@ -1,32 +1,21 @@
 const express = require('express')
 const router = express.Router()
 
+const moment = require('moment')
+
 const pool = require('../../../utils/db')
 
-router.get('/:companyName/:roomName', async (req, res, next) => {
-  let [results] = await pool.execute(
-    'SELECT  hotel_room_list.id AS hotel_room_list_id,hotel_room_list.*, room_service_list.*,room_service_list.id AS room_service_id,hotel_account.company_name,hotel_account.address FROM hotel_room_list INNER JOIN room_service_list ON hotel_room_list.room_name=room_service_list.room JOIN hotel_account ON hotel_account.company_name=hotel_room_list.hotel_name WHERE hotel_room_list.hotel_name=? AND room_service_list.hotel=? AND hotel_room_list.room_name=?',
-    [req.params.companyName, req.params.companyName, req.params.roomName]
-  )
-  // NOTE  JOIN了 訂單房型飯店 ，要想怎麼傳值到CheckOut
-  //  SELECT order_list_detail.*, total_order_list.*,hotel_room_list.*, room_service_list.*,hotel_account.company_name,hotel_account.address FROM total_order_list INNER JOIN order_list_detail ON total_order_list.id=order_list_detail.order_id JOIN hotel_room_list ON order_list_detail.product_id=hotel_room_list.id JOIN room_service_list ON room_service_list.id=hotel_room_list.id JOIN hotel_account ON hotel_room_list.hotel_name=hotel_account.company_name WHERE  total_order_list.user_email='lpy102817@gmail.com' AND hotel_room_list.id='4' AND total_order_list.order_date='2023-02-05 16:25:19';';
-  console.log('companyName', req.params.companyName)
-  console.log('roomName', req.params.companyName)
-  //   let [results2] = await pool.execute(
-  //     'SELECT * FROM hotel_service_list WHERE hotel_service_list.hotel=?',
-  //     [req.params.companyName]
-  //   )
-  res.json(results)
-})
-router.get('/:user', async (req, res, next) => {
-  let [results] = await pool.execute(
-    'SELECT user_credit_card.*, users.* FROM user_credit_card INNER JOIN users ON user_credit_card.user_email=users.email  WHERE user_credit_card.user_email=? ',
-    [req.params.user]
+router.use('/coupon/:couponId', async (req, res, next) => {
+  console.log('123')
+  let [useCoupon] = await pool.execute(
+    'SELECT user_coupon.id,user_coupon.discount FROM `user_coupon`  WHERE id=? ',
+    [req.params.couponId]
   )
 
-  res.json(results)
+  res.json(useCoupon)
 })
-router.post('/order', async (req, res) => {
+
+router.post('/order', async (req, res, next) => {
   console.log('POST /api/order', req.body)
   // req.body.stockId, req.body.stockName
   const memo = '無'
@@ -35,7 +24,7 @@ router.post('/order', async (req, res) => {
     { booker: req.body.formData, memo: memo },
   ])
   console.log('======description======', description)
-  let [results] = await pool.query(
+  let [resultsTotalOrderList] = await pool.query(
     'INSERT INTO `total_order_list` (`id`, `user_email`, `order_date`, `total_price`, `total_amount`, `state`, `valid`) VALUES (?, ?, ?, ?, ?, ?, ?)',
     [
       order_id,
@@ -48,7 +37,7 @@ router.post('/order', async (req, res) => {
     ]
   )
   console.log('amount', req.body.amount)
-  let [details] = await pool.query(
+  let [OrderListDetails] = await pool.query(
     'INSERT INTO `order_list_detail` (`id`, `order_id`, `product_id`, `company_name`, `price`, `amount`, `start_date`, `end_date`, `description`, `discount`, `vaild`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);',
     [
       null,
@@ -64,8 +53,40 @@ router.post('/order', async (req, res) => {
       1,
     ]
   )
+  let [useCoupon] = await pool.query(
+    'UPDATE `user_coupon` SET `valid` = ? WHERE `user_coupon`.`id` = ?;',
+    [0, req.body.discountId]
+  )
   //   INSERT INTO `order_list_detail` (`id`, `order_id`, `product_id`, `price`, `amount`, `start_date`, `end_date`, `description`, `discount`, `vaild`) VALUES (NULL, 'H1675571798103', '3', '1000', '10', '2023-02-01', '2023-02-02', '{booker: {lastName: \'張\',firstName: \'三明\',email: \'Chung1125@wanggg.com\',tel: \'0913255468\',country: \'台灣\',lang: \'中文\',},memo:\'房間不要有頭髮\'}', '20', '1');
   // console.log(results);
   res.json({})
+})
+router.use('/:companyName/:roomName', async (req, res, next) => {
+  let [results] = await pool.execute(
+    'SELECT  hotel_room_list.id AS hotel_room_list_id,hotel_room_list.*, room_service_list.*,room_service_list.id AS room_service_id,hotel_account.company_name,hotel_account.address FROM hotel_room_list INNER JOIN room_service_list ON hotel_room_list.room_name=room_service_list.room JOIN hotel_account ON hotel_account.company_name=hotel_room_list.hotel_name WHERE hotel_room_list.hotel_name=? AND room_service_list.hotel=? AND hotel_room_list.room_name=?',
+    [req.params.companyName, req.params.companyName, req.params.roomName]
+  )
+  // NOTE  JOIN了 訂單房型飯店 ，要想怎麼傳值到CheckOut
+  //  SELECT order_list_detail.*, total_order_list.*,hotel_room_list.*, room_service_list.*,hotel_account.company_name,hotel_account.address FROM total_order_list INNER JOIN order_list_detail ON total_order_list.id=order_list_detail.order_id JOIN hotel_room_list ON order_list_detail.product_id=hotel_room_list.id JOIN room_service_list ON room_service_list.id=hotel_room_list.id JOIN hotel_account ON hotel_room_list.hotel_name=hotel_account.company_name WHERE  total_order_list.user_email='lpy102817@gmail.com' AND hotel_room_list.id='4' AND total_order_list.order_date='2023-02-05 16:25:19';';
+  console.log('companyName', req.params.companyName)
+  console.log('roomName', req.params.companyName)
+  //   let [results2] = await pool.execute(
+  //     'SELECT * FROM hotel_service_list WHERE hotel_service_list.hotel=?',
+  //     [req.params.companyName]
+  //   )
+  res.json(results)
+})
+router.use('/:userEmail', async (req, res, next) => {
+  console.log('=====', req.params.userEmail)
+  const now = moment(Date.now()).format('YYYY-MM-DD')
+  // const now = '2023-03-21'
+
+  let [userCoupons] = await pool.execute(
+    'SELECT user_coupon.id,user_coupon.coupon_name,user_coupon.expire_date,user_coupon.discount FROM user_coupon  WHERE name= ?',
+    [req.params.userEmail]
+  )
+  console.log('req.body.userEmail', now)
+
+  res.json(userCoupons)
 })
 module.exports = router
